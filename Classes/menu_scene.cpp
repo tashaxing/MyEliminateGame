42f0c6cb868df26bf4aaae37c05f2c11ea0e3d26
tashaxing <tashaxing123@163.com>
2018-06-26 10:15:40 +0800

    add bg

--- Classes/menu_scene.cpp
+++ Classes/menu_scene.cpp
@@ -3,6 +3,8 @@
 
 USING_NS_CC;
 
+
+
 Scene *MenuScene::createScene()
 {
 	return MenuScene::create();
@@ -12,4 +14,15 @@ bool MenuScene::init()
 {
 	if (!Scene::init())
 		return false;	
+
+	// 获得屏幕尺寸常量(必须在类函数里获取)
+	const Size kScreenSize = Director::getInstance()->getVisibleSize();
+	const Vec2 kScreenOrigin = Director::getInstance()->getVisibleOrigin();
+
+	// 加载菜单页面背景
+	auto menu_background = Sprite::create("diamond.png");
+	menu_background->setPosition(kScreenOrigin.x + kScreenSize.width / 2, kScreenOrigin.y + kScreenSize.height / 2);
+	addChild(menu_background, 0);
+
+	return true;
 }
